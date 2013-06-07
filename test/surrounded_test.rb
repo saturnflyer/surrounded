8b1bd13a073798ff2fcf7f02531bc06af6bd4e44
Guille Carlos <guille@bitpop.in>
2013-06-07 10:26:13 -0400

    Add initial test - Simple but gets us started

---
+++ test/surrounded_test.rb
@@ -0,0 +1,39 @@
+require 'test_helper'
+require 'surrounded'
+require 'surrounded/context'
+
+class User
+  include Surrounded
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+end
+
+class SampleUseCase
+  extend Surrounded::Context
+
+  setup(:user, :other_user)
+
+  trigger :be_polite do
+    user.say_hello
+  end
+
+  module User
+    def say_hello
+      "Hi #{other_user.name}, I am #{user.name}"
+    end
+  end
+end
+
+describe "An evironment" do
+  user       = User.new("Jim")
+  other_user = User.new("Guille")
+
+  it "should have certain objects inside" do
+
+    use_case = SampleUseCase.new(user, other_user)
+    assert use_case.be_polite == "Hi #{other_user.name}, I am #{user.name}"
+  end
+end
