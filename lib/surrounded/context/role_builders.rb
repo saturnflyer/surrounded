f407659bcd40742010332308a88ab81d9cdc8956
Jim Gay <jim@saturnflyer.com>
2014-11-20 15:36:47 -0500

    DRY up the conversion of role names to module names

--- lib/surrounded/context/role_builders.rb
+++ lib/surrounded/context/role_builders.rb
@@ -5,7 +5,7 @@ module Surrounded
       # Define behaviors for your role players
       def role(name, type=default_role_type, &block)
         if type == :module
-          mod_name = name.to_s.gsub(/(?:^|_)([a-z])/){ $1.upcase }
+          mod_name = RoleBuilders.mod_name(name)
           mod = Module.new(&block)
           mod.send(:include, ::Surrounded)
           private_const_set(mod_name, mod)
@@ -21,7 +21,7 @@ module Surrounded
       # Create a named behavior for a role using the standard library SimpleDelegator.
       def wrap(name, &block)
         require 'delegate'
-        wrapper_name = name.to_s.gsub(/(?:^|_)([a-z])/){ $1.upcase }
+        wrapper_name = RoleBuilders.mod_name(name)
         klass = private_const_set(wrapper_name, Class.new(SimpleDelegator, &block))
         klass.send(:include, Surrounded)
       end
@@ -30,7 +30,7 @@ module Surrounded
 
       # Create an object which will bind methods to the role player
       def interface(name, &block)
-        class_basename = name.to_s.gsub(/(?:^|_)([a-z])/){ $1.upcase }
+        class_basename = RoleBuilders.mod_name(name)
         interface_name = class_basename + 'Interface'
 
         behavior = private_const_set(interface_name, Module.new(&block))
@@ -42,6 +42,10 @@ module Surrounded
         end
       end
       
+      def self.mod_name(name)
+        name.to_s.gsub(/(?:^|_)([a-z])/){ $1.upcase }
+      end
+
     end
   end
 end
