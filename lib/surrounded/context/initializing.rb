27308dd2a564da8f80e241e5e84300d6ad59b469
Jim Gay <jim@saturnflyer.com>
2015-02-05 22:39:36 -0500

    simplify code for handling initialize arguments

--- lib/surrounded/context/initializing.rb
+++ lib/surrounded/context/initializing.rb
@@ -10,9 +10,8 @@ module Surrounded
         line = __LINE__
         mod.class_eval "
           def initialize(#{setup_args.join(',')})
-            arguments = method(__method__).parameters.map{|arg| eval(arg[1].to_s) }
             @role_map = RoleMap.new
-            map_roles(#{setup_args}.zip(arguments))
+            map_roles(#{setup_args.to_s}.zip([#{setup_args.join(',')}]))
           end
         ", __FILE__, line
         const_set("ContextInitializer", mod)
