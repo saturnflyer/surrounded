f6511c4796486f776665ed95797ebc554fd09eee
James Hamilton <wjdhamilton@hotmail.co.uk>
2016-03-18 16:22:29 +0000

    Redesigned on_name_collision and refactored NameCollisionHandler
    on_name_collision used to accept either a symbol or a block, and then handled the corresponding action with a case statement. This was unwieldy and unnecessarily complex. on_name_collision now only accepts a symbol which is the name of a method in the class which will be called when a name collision is detected.
    
    Also refactored the code to make it more legible and easier to read.

--- lib/surrounded/context/name_collision_detector.rb
+++ lib/surrounded/context/name_collision_detector.rb
@@ -10,37 +10,52 @@ module NameCollisionDetector
   end
 
   module NameCollisionHandler
+    
     def detect_collisions(role_object_map)
-      collision_map = Hash.new do |map, role|
-        map[role] = []
-      end
-      
-      collisions = check_for_collisions role_object_map, collision_map
+      collisions = check_for_collisions role_object_map, create_collision_map
       handle_collisions(collisions)
     end
     
     def handle_collisions(collisions)
+      handler = self.class.handler
       collisions.each_pair do |role, colliders|
-        self.send(self.class.handler, role, colliders) if self.class.handler
+        send(handler, role, colliders) if handler
       end
     end
 
     def check_for_collisions(role_map, collisions, index=0)
-      role_names = role_map.keys.dup
-      if index.eql? role_names.length
-        return collisions
+      role_names = list_role_names(role_map)
+      if all_roles_checked?(role_names, index)
+        return collisions 
+      else
+        expand_collision_map(collisions, next_role(role_names, index), role_names, role_map)
+        check_for_collisions role_map, collisions, (index += 1)
       end
-
-      candidate_role_name = role_names[index]
-      actor = role_map[candidate_role_name]
-      role_names.delete(candidate_role_name)
-      role_names.each do |role|
-        collisions[candidate_role_name] << role if actor.respond_to? role
-      end
-      index += 1
-      check_for_collisions role_map, collisions, index
     end
     
+    def expand_collision_map(collisions, candidate, role_names, role_map)
+      actor = role_map[candidate]
+      collisions_found = check_role_for_collisions(role_names, actor)
+      collisions[candidate] = collisions_found unless collisions_found.empty?
+    end
+    
+    def next_role(names, index)
+      names.delete_at index
+    end
+    
+    def check_role_for_collisions(role_names, actor)
+      role_names.each_with_object([]) do |role, collisions|
+        collisions << role if actor.respond_to? role
+      end
+    end
+    
+    def list_role_names(role_map)
+      role_map.keys.dup
+    end
+    
+    def all_roles_checked?(names, index)
+      index.eql? names.length
+    end
     
     def nothing(role, array); end;
       
@@ -50,6 +65,16 @@ module NameCollisionDetector
     
     def warn(role, array)
       puts "#{role} has name collisions with #{array}" if array.length > 0
+    end
+    
+    def handler
+      self.class.handler
+    end
+    
+    def create_collision_map
+      Hash.new do |map, role|
+        map[role] = []
+      end
     end
   end
 end
