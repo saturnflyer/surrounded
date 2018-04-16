9650826c40da76dcaed311915c05f75d41265590
Jim Gay <jim@saturnflyer.com>
2018-04-16 17:48:22 -0400

    Name the anonymous class containing role players.
    moves Forwardable def_delegators out of the class definition to make it clearer

--- lib/surrounded/context/role_map.rb
+++ lib/surrounded/context/role_map.rb
@@ -7,17 +7,21 @@ module Surrounded
 
       class << self
         def from_base(klass=::Triad)
-          role_mapper = Class.new(self)
-          Surrounded::Exceptions.define(role_mapper, exceptions: :ItemNotPresent, namespace: klass)
-          role_mapper.container_class=(klass)
-          role_mapper.def_delegators :container, :update, :each, :values, :keys
-          role_mapper
+          unless const_defined?(:Container)
+            role_mapper = Class.new(self)
+            role_mapper.container_class=(klass)
+            Surrounded::Exceptions.define(role_mapper, exceptions: :ItemNotPresent, namespace: klass)
+            const_set(:Container, role_mapper)
+          end
+          const_get(:Container)
         end
 
         def container_class=(klass)
           @container_class = klass
         end
       end
+
+      def_delegators :container, :update, :each, :values, :keys
 
       def container
         @container ||= self.class.instance_variable_get(:@container_class).new
@@ -29,7 +33,7 @@ module Surrounded
 
       def role_player?(object)
         !values(object).empty?
-      rescue ::StandardError
+      rescue self.container.class::ItemNotPresent
         false
       end
 
